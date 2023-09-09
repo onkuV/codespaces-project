8689a48b87e79f1ef0fda56a2d1b030a70ae2505
OnkuV <119777415+onkuV@users.noreply.github.com>
2023-09-09 13:57:00 +0000

    info change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a sysadmin. I enjoy setting up dev and production environments, and I'm always looking for new ways to improve my workflow. I'm also a developer. I enjoy building websites and applications that are accessible to everyone.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
